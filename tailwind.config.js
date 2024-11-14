a9a64f0df3db4bc10c229daaf93b83ef0457dd52
Nabin <nabinsth@gmail.com>
2024-11-14 21:05:45 +0545

    src and tailwindcss file update

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "/blog-preview-card-main/blog-preview-card-main/src/**/*.{html,js}",
+    "/blog-preview-card-main/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
