b3f8344cd27e9ed66260b573eeebae260c1aec22
Nabin <nabinsth@gmail.com>
2024-11-14 21:11:44 +0545

    index and tailwind.config file update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "/blog-preview-card-main/blog-preview-card-main/src/**/*.{html,js}",
-    "/blog-preview-card-main/**/*.{html,js}",
-  ],
+  content: ["/src/**/*.{html,js}", "/**/*.{html,js}"],
   theme: {
     extend: {},
   },
