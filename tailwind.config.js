ad1e960f2b7bad60b1f51e1e0dd76750a6224a86
Nabin <nabinsth@gmail.com>
2024-11-16 12:28:57 +0545

    index and tailwind file update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/src/**/*.{html,js}", "/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        "3xl": "8px 8px 0px rgba(0, 0, 0, 100)",
+      },
+    },
+    screens: {
+      mobile: "375px",
+      // => @media (min-width: 375px) {...}
+      desktop: "1440px",
+      // => @media (min-width: 1440px) {...}
+    },
   },
   plugins: [],
 };
